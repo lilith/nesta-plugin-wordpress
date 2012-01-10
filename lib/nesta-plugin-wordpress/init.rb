5abc3af373762e45cc0091f787c97400bebec025
Nathanael Jones <nathanael.jones@gmail.com>
2012-01-10 18:13:47 -0500

    Added RSS feed and attachment redirection.

--- lib/nesta-plugin-wordpress/init.rb
+++ lib/nesta-plugin-wordpress/init.rb
@@ -2,7 +2,23 @@ module Nesta
   module Plugin
     module Wordpress
       module Helpers
-        # If your plugin needs any helper methods, add them here...
+        #Rewite all requests for /wp-content/ to /attachments/wp-content
+        before '/wp-content/*' do
+          request.path_info = "/attachments" + path_info
+        end
+        
+        get '/feed/' do
+           redirect '/articles.xml', 301
+        end
+        
+        get '/feed/' do
+           redirect '/articles.xml', 301
+        end
+        
+        get '/:id/:article/feed/' do
+          #TODO, look up article and redirect to intensedebate feed
+        end
+        
       end
     end
   end
